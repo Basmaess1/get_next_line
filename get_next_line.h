7504d414227352405364ce2bbc01d06803a6c2a7
Basma <essabribassma062@gmail.com>
2024-12-28 11:03:41 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,27 +6,25 @@
 /*   By: bessabri <bessabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 17:25:27 by bessabri          #+#    #+#             */
-/*   Updated: 2024/12/27 22:08:09 by bessabri         ###   ########.fr       */
+/*   Updated: 2024/12/28 10:57:45 by bessabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h> 
-#include <unistd.h>
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 1024
-#endif
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h> 
+# include <unistd.h>
 
-char    *get_next_line(int fd);
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+char	*get_next_line(int fd);
 char	*ft_strdup(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
-
-
-
-
+char	*achb9a(char *res);
 #endif
