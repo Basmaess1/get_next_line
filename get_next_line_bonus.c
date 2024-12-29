cf6afdffdb806227c37b290e7173e81789e36438
Basma <essabribassma062@gmail.com>
2024-12-29 11:49:55 +0100

    done

---
+++ get_next_line_bonus.c
@@ -0,0 +1,146 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bessabri <bessabri@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/21 17:24:18 by bessabri          #+#    #+#             */
+/*   Updated: 2024/12/29 11:39:54 by bessabri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+static char	*read_function(int fd, char *res)
+{
+	ssize_t	n;
+	char	*buffer;
+	char	*tmp;
+
+	buffer = malloc((size_t)(BUFFER_SIZE)+1);
+	if (!buffer)
+		return (0);
+	n = 1;
+	while (!ft_strchr(res, '\n') && n != 0)
+	{
+		n = read(fd, buffer, BUFFER_SIZE);
+		if (n == -1)
+		{
+			return (free(buffer), free(res), NULL);
+		}
+		buffer[n] = '\0';
+		tmp = ft_strjoin(res, buffer);
+		if (!tmp)
+			return (free(buffer), free(res), NULL);
+		free(res);
+		res = tmp;
+	}
+	free(buffer);
+	return (res);
+}
+
+static char	*handle_newline(char *res)
+{
+	char	*newline;
+	int		i;
+
+	i = 0;
+	while (res[i] && res[i] != '\n')
+		i++;
+	newline = malloc(i + 2);
+	if (!newline)
+		return (free(res), NULL);
+	i = 0;
+	while (res[i] && res[i] != '\n')
+	{
+		newline[i] = res[i];
+		i++;
+	}
+	if (res[i] == '\n')
+	{
+		newline[i] = res[i];
+		i++;
+	}
+	newline[i] = '\0';
+	return (newline);
+}
+
+static char	*achb9a(char *res)
+{
+	int		i;
+	int		j;
+	char	*lib9a;
+
+	i = 0;
+	while (res[i] && res[i] != '\n')
+		i++;
+	if (res[i] == '\0' || res[i + 1] == '\0')
+	{
+		free(res);
+		return (NULL);
+	}
+	lib9a = malloc(ft_strlen(res) - i + 1);
+	if (!lib9a)
+		return (free(res), NULL);
+	j = 0;
+	i++;
+	while (res[i])
+	{
+		lib9a[j++] = res[i++];
+	}
+	lib9a[j] = '\0';
+	free(res);
+	return (lib9a);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*s[1024];
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd >= 1024)
+	{
+		return (NULL);
+	}
+	s[fd] = read_function(fd, s[fd]);
+	if (!s[fd] || s[fd][0] == '\0')
+	{
+		free(s[fd]);
+		s[fd] = NULL;
+		return (NULL);
+	}
+	line = handle_newline(s[fd]);
+	if (!line)
+		return (free(s[fd]), s[fd] = NULL, NULL);
+	s[fd] = achb9a(s[fd]);
+	return (line);
+}
+// int main() {
+//     int fd1 = open("file1.txt", O_RDONLY);
+//     int fd2 = open("file2.txt", O_RDONLY);
+// 	int fd3 = open("file3.txt", O_RDONLY);
+// 	if (fd1 < 0 || fd2 < 0 || fd3 < 0)
+// 	{
+// 	perror("Error opening file");
+// 	return 1;
+// 	}
+//     char *line;
+
+//     while ((line = get_next_line(fd1))) {
+//         printf("%s",line);
+//         free(line);
+//     }
+//     while ((line = get_next_line(fd2))) {
+//         printf("%s",line);
+//         free(line);
+//     }
+// 	    while ((line = get_next_line(fd3))) {
+//         printf("%s",line);
+//         free(line);
+//     }
+//     close(fd1);
+//     close(fd2);
+// 	close(fd3);
+//     return 0;
+// }
